26f6ae8c5bc2ff4ae2ff30eadad78efd8622ed20
Ahmed Eldessouki <ahmed.dosoky1982@gmail.com>
2022-11-24 23:36:13 +0400

    edit the mui them for top bar

--- ptw-dash/src/App.js
+++ ptw-dash/src/App.js
@@ -6,33 +6,41 @@ import './app.scss';
 import Requist from './components/requists/Requist';
 import Popup from './components/Popup/Popup';
 //import MUI theme
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './components/theme/theme';
 
 export default function App() {
+  let [theme, colorMode] = useMode();
   return (
     <>
-      {/*Navbar component*/}
-      <Navbar />
-      {/*card component*/}
-      <div className='app__cards'>
-        {
-          CardData.map (
-            (Card,i) => {
-              return (
-                <Cards
-                key = {i}
-                title = {Card.title}
-                color = {Card.color}
-                icon = {Card.icon}
-                />
+      <ColorModeContext.Provider value = {colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          {/*Navbar component*/}
+          <Navbar />
+          {/*card component*/}
+          <div className='app__cards'>
+            {
+              CardData.map (
+                (Card,i) => {
+                  return (
+                    <Cards
+                    key = {i}
+                    title = {Card.title}
+                    color = {Card.color}
+                    icon = {Card.icon}
+                    />
+                  )
+                }
               )
             }
-          )
-        }
-      </div>
-      {/*requists component*/}
-      <Requist />
-      {/*popup component */}
-      <Popup />
+          </div>
+          {/*requists component*/}
+          <Requist />
+          {/*popup component */}
+          <Popup />
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
   );
 }
--- ptw-dash/src/components/navbar/Navbar.jsx
+++ ptw-dash/src/components/navbar/Navbar.jsx
@@ -3,19 +3,48 @@ import './navbar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome , faInfoCircle , faUserNinja, faBars , faFloppyDisk} from "@fortawesome/free-solid-svg-icons";
 import SearchBar from '../SearchBar/SearchBar';
+import {Box, TextField, useTheme} from '@mui/material';
+import { tokens } from '../theme/theme';
+import { height, padding } from '@mui/system';
+
 
 export default function Navbar () {
-
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
+    
     return (
         <>
-            <div className='navbar__container'>
-                <SearchBar className="SearchBar"/>
-                <FontAwesomeIcon icon= { faFloppyDisk } className = "navbar__container__icon"/>
-                <FontAwesomeIcon icon= { faHome } className = "navbar__container__icon"/>
-                <FontAwesomeIcon icon= { faInfoCircle } className = "navbar__container__icon"/>
-                <FontAwesomeIcon icon= { faUserNinja } className = "navbar__container__icon"/>
-                <FontAwesomeIcon icon= { faBars } className = "navbar__container__icon"/>
-            </div>
+            <Box 
+            display={"flex"}
+            width = {"100hv"}
+            height = {"3rem"}
+            justifyContent = {"space-between"}
+            alignItems = {"center"}
+            sx = {{
+                backgroundColor: colors.primary[500]
+            }}>
+                <TextField
+                label = {"search ..."}
+                sx = {{
+                    height: "1rem",
+                    padding: "0",
+                }}
+                />
+
+                <Box 
+                display = {"flex"}
+                width = {"15rem"}
+                justifyContent = {"space-between"}
+                fontSize = {"25px"}
+                padding = {"10px"}
+                >
+                    <FontAwesomeIcon icon= { faFloppyDisk } className = "navbar__container__icon"/>
+                    <FontAwesomeIcon icon= { faHome } className = "navbar__container__icon"/>
+                    <FontAwesomeIcon icon= { faInfoCircle } className = "navbar__container__icon"/>
+                    <FontAwesomeIcon icon= { faUserNinja } className = "navbar__container__icon"/>
+                    <FontAwesomeIcon icon= { faBars } className = "navbar__container__icon"/>
+                </Box>
+            </Box>
         </>
     )
 }
